1fdc89ef12097e015decfe7b3f3f1c4a8883aed2
sidsakhala <sakhalasudhanshu@gmail.com>
2026-01-28 23:02:53 +0530

    orchestated docker using k8s

--- k8s-apply.sh
+++ k8s-apply.sh
@@ -7,7 +7,7 @@ echo "Applying Kubernetes Deployment..."
 kubectl apply -f deployment.yaml
 
 echo "Applying Kubernetes Service..."
-kubectl apply -f service.yaml
+kubectl apply -f k8s-service.yml
 
 echo "Scaling deployment to 3 replicas..."
 kubectl scale deployment django-mlflow-deployment --replicas=3
